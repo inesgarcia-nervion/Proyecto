e172f72bbbed48be23d17d9fa80e914bddf30e6d
inesgarcia-nervion <ines.garcia@iesnervion.es>
2025-10-21 14:39:29 +0200

    Apuntes

--- api_users/users.py
+++ api_users/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
@@ -25,8 +25,25 @@ def users():
 
 @app.get("/users/{id_user}")              #El nombre nos lo podemos inventar
 def get_user(id_user : int):              #El nombre tiene que ser igual al de arriba SIEMPRE
-    users = [user for user in users_list if user.id == id_user]  #en user.id, el id hace referencia a la clase creada arriba
+    users = search_user(id_user)
+    return 
     
     
-    return users[0] if users else {"error" : "User not found"}    #Si la longitud de la lista es distinta a 0
+@app.get("/users/")       
+def get_user(id_user : int):
+    return search_user(id)
+
+
+def search_user(id : int):        
+    #  buscamos usuario por id en la lista
+    # devuelve una lista vacía si no encuentra nada
+    # devuelve una lista con el usuario encontrado
+    users = [user for user in users_list if user.id == id]  #en user.id, el id hace referencia a la clase creada arriba
+   
+    # devolvemos la primera posición de la lista 
+    
+    if users:
+        return users[0]
+    raise HTTPException(status_code=404, detail="User not found")  
     # También se puede hacer: return users[0] if len(users) != 0 else {"error" : "User not found"}
+    
